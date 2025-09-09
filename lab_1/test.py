8d1271037987a71890d57c3e2db15d0ee5975ff2
sookolls <olgasokol033@gmail.com>
2025-09-09 17:56:08 +0300

    задача 2

--- lab_1/test.py
+++ lab_1/test.py
@@ -1,23 +1,11 @@
-def marks (mark):
-    assert isinstance (mark, int) , "Оценка должна быть целым числом"
-    assert 0 <= mark <= 100, "Оценка должна быть между 0 и 100"
-    if mark <0 or mark > 100:
-        return 'incorrect value'
-    elif mark >=90:
-        return 'A'
-    elif 80<= mark <= 89:
-        return 'B'
-    elif  70<= mark <= 79:
-        return 'C'
-    elif 60<= mark <= 69:
-        return 'D'
-    elif mark <= 59:
-        return 'E'
+def leap_year(year):
+    assert isinstance (year, int)
+    if (year//100)!=0 and year // 4 or year//400:
+        return "Год високосный"
+    elif print ("not leap_year"):
+        return "Год невисокосный"
 try:
-    mark = int(input("Введите число: "))
-    print(marks(mark))
+    year = int(input("Введите число: "))
+    print(leap_year(year))
 except ValueError:
     print("Некорректный тип")
-except AssertionError as e:
-    print(f"Неверное значение: {e}")
-
